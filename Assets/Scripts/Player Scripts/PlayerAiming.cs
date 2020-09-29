da0d3c9d943bf77896a9df9a2dc22e569a70e338
Trichardson255 <71813444+Trichardson255@users.noreply.github.com>
2020-09-29 20:16:44 +1000

    Updated 29/09/2020

--- Assets/Scripts/Player Scripts/PlayerAiming.cs
+++ Assets/Scripts/Player Scripts/PlayerAiming.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class PlayerAiming : MonoBehaviour
 {
+    LayerMask layerMask;
+
+    private void Awake()
+    {
+        layerMask = LayerMask.GetMask("Ground");
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Input.mousePosition, Vector3.up);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+        {
+            transform.LookAt(hit.point);
+        }
     }
 }
